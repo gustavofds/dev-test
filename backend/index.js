b2423cf8b99ff7773ac08f42ec4d2ec90927752b
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-08 17:54:28 -0300

    Initial setup of user routes and controller

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.status(200).send('OK');
 });
 
-const port = process.env.PORT || 8000;
+app.use('/api/users', userRouter);
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
