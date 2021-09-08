6a23b0a8270af67412ba1e78d23ea596a1c18c2c
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-08 18:10:44 -0300

    fix: Corrects model functions placement

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -6,34 +6,3 @@ exports.getAll = async () => {
 
   return users;
 };
-
-exports.getUserById = async (id) => {
-  if(!ObjectId.isValid(id)) return null;
-
-  const db = await connection();
-
-  const user = await db.collection('users').findOne({ _id: ObjectId(id) });
-
-  return user || null;
-};
-
-exports.getUserByEmail = async (email) => {
-  const db = await connection();
-
-  const user = await db.findOne({ email });
-
-  return user || null;
-}
-
-exports.createUser = async({ fullName, email, password }) => {
-  const db = await connection();
-
-  const user = await db.collection('users').insertOne({ fullName, email, password });
-
-  return {
-    id: user.insertedId,
-    firstName,
-    lastName,
-    email,
-  };
-};
