67c78e737486e659be35df91f4d1d498fe0e3b4b
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-08 18:07:20 -0300

    Creates model methods getUserByEmail and createUser

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,7 +1,27 @@
+const { ObjectId } = require('mongodb');
 const User = require('../models/User');
 
 exports.getAll = async () => {
   const users = await User.getAll();
 
   return users;
+};
+
+exports.getUserByEmail = async (email) => {
+  const users = await User.findOne({ email });
+
+  return users;
+}
+
+exports.createUser = async({ fullName, email, password }) => {
+  const db = await connection();
+
+  const user = await db.collection('users').insertOne({ fullName, email, password });
+
+  return {
+    id: user.insertedId,
+    firstName,
+    lastName,
+    email,
+  };
 };
