9a87555c2e17d0bc6ff4611a52bbec2c1a82fbb9
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-09 12:21:05 -0300

    Adds cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 const userRouter = require('./routes/userRouter');
 const transactionRouter = require('./routes/transactionRouter');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
